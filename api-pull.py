58e5ed2e54eb75ef794772b567f82181ffeb1869
tenderribs <dc3dude@gmail.com>
2024-09-15 15:23:47 +0000

    save sites and loggers

--- api-pull.py
+++ api-pull.py
@@ -3,46 +3,83 @@
 import requests
 
 from db.env import env, check_env, db_url
-from db.models import Site
+from db.models import Site, Logger
+from db.upsert import upsert_loggers, upsert_sites
 
 from TokenHelper import TokenHelper
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, session
+
+# COOLDOWN_S = 1  # avoid HTTP 429s by waiting between requests
 
 
-def innet_main(session) -> None:
-    """https://portal.dev.innet.io"""
-
-    # Get auth token
-    helper = TokenHelper(f"https://id.{env["INNET_HOST"]}", env["INNET_CLIENT_NAME"], env["INNET_CLIENT_SECRET"])
-    token, expires_at = helper.get_token()
-    auth_header = {"Authorization": "Bearer " + token}
-
-    # Fetch list of sites
-    sites = requests.request("GET", f"https://meta.{env["INNET_HOST"]}/v1/site", headers=auth_header, params={
-        "project_id": env["INNET_PROJECT_ID"], # set in .env
-        "exclude_historic": False,
-        "exclude_prepared": False,
-    })
-
-    print("Sites:")
-    for site in sites.json():
-        print(site)
-
-    return
-
-    response = requests.request(
+def refresh_innet_sites(session: session, auth_header: dict) -> None:
+    """Get an updated list of sites"""
+    sites = requests.request(
         "GET",
-        f"https://data.{env["INNET_HOST"]}/v1/timeseries/measurements",
-        params={
-            'site_id': 'BueroMEL'
-        },
+        f"https://meta.{env["INNET_HOST"]}/v1/site",
         headers=auth_header,
+        params={
+            "project_id": env["INNET_PROJECT_ID"],  # set in .env
+            "exclude_historic": False,
+            "exclude_prepared": False,
+        },
     )
 
-    print("\nDATA:")
-    print(response.json())
+    upsert_sites(session, sites.json())
+
+
+def fetch_active_site_data(
+    session: session, auth_header: dict, active_sites: list[Site]
+):
+    for site in active_sites:
+        # get the loggers related to each site
+        logger_installations = requests.request(
+            "GET",
+            f"https://meta.{env["INNET_HOST"]}/v1/site/{site.id}/logger_installations",
+            params={"site_id": site.id},
+            headers=auth_header,
+        )
+
+        # then save the loggers in the DB
+        upsert_loggers(session, logger_installations.json())
+
+    loggers = session.query(Logger).filter_by(active=True).all()
+
+    print("Fetching active loggers")
+
+    for logger in loggers:
+        data = requests.request(
+            "GET",
+            f"https://data.{env["INNET_HOST"]}/v1/timeseries/measurements",
+            params={"logger_id": logger.id},
+            headers=auth_header,
+        )
+
+        print(data.json(), "\n\n")
+
+
+def innet_main(session: session) -> None:
+    """https://portal.dev.innet.io"""
+
+    # Get authentication token
+    helper = TokenHelper(
+        f"https://id.{env["INNET_HOST"]}",
+        env["INNET_CLIENT_NAME"],
+        env["INNET_CLIENT_SECRET"],
+    )
+    token, expires_at = (
+        helper.get_token()
+    )  # assume expiration long after script has finished executing
+    auth_header = {"Authorization": "Bearer " + token}
+
+    refresh_innet_sites(session, auth_header)
+
+    active_sites: list(Site) = (
+        session.query(Site).filter_by(provider_id="innet", active=True).all()
+    )
+    fetch_active_site_data(session, auth_header, active_sites)
 
 
 if __name__ == "__main__":
@@ -50,7 +87,11 @@ if __name__ == "__main__":
         check_env()
 
         engine = create_engine(db_url)
-        session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+        Session: sessionmaker = sessionmaker(
+            autocommit=False, autoflush=False, bind=engine
+        )
+        session: session = Session()
 
         innet_main(session)
 
--- db/models.py
+++ db/models.py
@@ -17,21 +17,25 @@ class Provider(Base):
 
     sites = relationship("Site", back_populates="provider")
 
+    def __repr__(self) -> str:
+        return f"Provider(id={self.id!r}"
+
 
 class Site(Base):
     __tablename__ = "site"
-
-    def __init__(self, _id):
-        self.id = _id
 
     id = Column(String, primary_key=True, index=True)
     name = Column(String, nullable=True)
     provider_id = Column(String, ForeignKey("provider.id"))
     WGS84_lat = Column(Float, nullable=False)
     WGS84_lon = Column(Float, nullable=False)
+    active = Column(Boolean, default=False, nullable=False)
 
     provider = relationship("Provider", back_populates="sites")
     loggers = relationship("Logger", back_populates="site")
+
+    def __repr__(self) -> str:
+        return f"Site(id={self.id!r}"
 
 
 class Logger(Base):
@@ -39,11 +43,15 @@ class Logger(Base):
 
     id = Column(String, primary_key=True, index=True)
     name = Column(String, nullable=True)
-    active = Column(Boolean, nullable=False)
+    active = Column(Boolean, nullable=False, default=False)
     site_id = Column(String, ForeignKey("site.id"))
+    interval_s = Column(Integer, nullable=False)
 
     site = relationship("Site", back_populates="loggers")
     data = relationship("LoggerData", back_populates="logger")
+
+    def __repr__(self) -> str:
+        return f"Logger(id={self.id!r}"
 
 
 class LoggerData(Base):
