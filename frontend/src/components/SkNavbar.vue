d46ed945c3e2d55d5ba5669cad972c8ee6852da1
tenderribs <dc3dude@gmail.com>
2024-10-17 16:52:25 +0200

    get basic defaults ready

---
+++ frontend/src/components/SkNavbar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="border-b-[1.5px] border-b-gray-200 py-2 mb-2">
+    <div class="container flex flex-row justify-between">
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link :to="{ name: 'login' }">Login</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Home</div>
+  <div>Login</div>
 </template>
 
 <style scoped></style>
