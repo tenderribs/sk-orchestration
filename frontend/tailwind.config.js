8a7f4f62d654e3b1a3f0237d7a6a729c70917f66
tenderribs <dc3dude@gmail.com>
2024-10-18 14:18:35 +0200

    add fonts like ugz

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,9 +4,17 @@ export default {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'primary': 'rgb(15, 5, 160)',
+        'secondary': 'rgb(115, 115, 115)',
+      },
+      fontFamily: {
+        sans: ['HelveticaNeue', 'sans-serif']
+      }
     },
     container: {
       center: true,
+
     },
   },
   plugins: [],
