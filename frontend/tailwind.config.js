f1d9a6eeb99cb211d3f621c6f9c93b958f92fd76
tenderribs <dc3dude@gmail.com>
2024-10-18 10:51:15 +0200

    config fixes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
     },
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
-      },
     },
   },
   plugins: [],
