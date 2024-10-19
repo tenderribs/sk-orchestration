25bcf8a4f12463ccc091c5a7b1f84abb05013872
tenderribs <dc3dude@gmail.com>
2024-10-19 12:37:55 +0200

    add models

---
+++ frontend/src/models/measurement.ts
@@ -0,0 +1,21 @@
+enum MeasurementType {
+    WIND_SPEED_MS = "ws_ms",
+    WIND_SPEED_MAX_MS = "ws_max_ms",
+    EAST_WIND_SPEED_MS = "e_ws_ms",
+    NORTH_WIND_SPEED_MS = "n_ws_ms",
+    REL_HUMIDITY_PCT = "rel_h_pct",
+    IRRADIATION_WM2 = "irr_wm2",
+    WIND_DIRECTION_DEG = "w_dir_deg",
+    ATM_PRESSURE_HPA = "atm_p_hpa",
+    AIR_TEMPERATURE_C = "air_t_c",
+    BATTERY_VOLTAGE_V = "bat_v",
+    PRECIPITATION_MM = "precip_mm",
+    DEWPOINT_C = "dewpoint_t_c",
+    GLOBAL_RADIATION_WM2 = "global_rad_wm2",
+}
+
+export type Measurement = {
+    meas_type: MeasurementType;
+    value: number;
+    timestamp: Date;
+}
---
+++ frontend/src/models/site.ts
@@ -0,0 +1,16 @@
+enum Provider {
+    UGZ = "UGZ",
+    INNET = "INN",
+    METEOBLUE = "MET",
+    AWEL = "AWE",
+}
+
+export type Site = {
+    id: number;
+    provider: Provider;
+    name: string;
+    wgs84_lat: number;
+    wgs84_lon: number;
+    masl: number;
+    magl?: number;
+}
