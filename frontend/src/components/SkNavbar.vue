9e39f957a19cf96a98c6b0a5eb1874448b2a128a
tenderribs <dc3dude@gmail.com>
2024-10-24 10:20:35 +0200

    use logo from ugz

--- frontend/src/components/SkNavbar.vue
+++ frontend/src/components/SkNavbar.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
 import { isLoggedIn } from '@/helpers/useIsLoggedIn'
+import { onMounted } from 'vue'
+
+const { BASE_URL } = import.meta.env
+
+onMounted(() => console.log(BASE_URL))
 </script>
 
 <template>
     <div class="border-b-[1.5px] border-b-gray-200 py-2.5 mb-6">
-        <div class="container flex flex-row justify-between">
-            <div>
-                <router-link class="font-bold" :to="{ name: 'home' }">UGZ Sensors</router-link>
+        <div class="container flex flex-row justify-between items-center">
+            <div class="flex flex-row items-center">
+                <router-link :to="{ name: 'home' }">
+                    <img class="h-10" :src="BASE_URL + 'logo_stzh_ugz_rgb_blau_digital.svg'" />
+                </router-link>
+                <!-- <router-link class="pl-4 font-md text-xl" :to="{ name: 'home' }"
+                    >Sensors</router-link
+                > -->
             </div>
             <div v-if="isLoggedIn">
-                <router-link class="pr-8" :to="{ name: 'sites' }">Dashboard</router-link>
-                <router-link class="pr-8" :to="{ name: 'sites' }">Sites</router-link>
+                <router-link class="pr-8" :to="{ name: 'home' }">Dashboard</router-link>
                 <router-link class="pr-8" :to="{ name: 'sites' }">Installations</router-link>
-                <router-link :to="{ name: 'sites' }">Loggers</router-link>
+                <router-link class="pr-8" :to="{ name: 'sites' }">Sites</router-link>
+                <router-link :to="{ name: 'hardware' }">Hardware</router-link>
             </div>
             <div>
                 <router-link v-if="!isLoggedIn" :to="{ name: 'login' }">Login</router-link>
